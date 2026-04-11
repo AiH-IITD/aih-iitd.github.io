d6172d2b9ae8bdad2bbb1f336d1fe8b3e7d47508
SSKlearns <sarveshshashikumar0908@gmail.com>
2026-04-11 17:29:35 +0530

    Update google search index verification.

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,9 @@ export const metadata: Metadata = {
   description:
     "Advancing healthcare through cutting-edge computer vision and AI research. Developing innovative solutions for medical imaging, diagnosis, and patient care.",
   keywords: "AI, healthcare, computer vision, medical imaging, research lab, machine learning",
+  verification: {
+    google: 'mx9dfIR0c9SXawFRl9wkowC-4j_iX_Lx0dM9TlKya88',    
+  }
 }
 
 export default function RootLayout({
